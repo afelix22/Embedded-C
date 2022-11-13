1aa06e94dfbed9111448e15a7b955cc749985d04
Frame Skip <arnoldfelix2017@gmail.com>
2022-11-14 06:13:33 +0800

    Lab 03

--- Lab_03_Embedded_C_Functions/inLabAssignment3.c
+++ Lab_03_Embedded_C_Functions/inLabAssignment3.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int isLeapYear(int year){
-    if (year % 400 == 0){
-        return 1;
-    }else if (year % 4 == 0){
+    if (year % 400 == 0 || year % 4 == 0){
         return 1;
     }else if (year % 100 == 0){
         return 0;
